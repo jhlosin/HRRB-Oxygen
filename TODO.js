fee9bf9ad001a26c638b66445780313b57d49115
Peter Park <jhlosin@gmail.com>
2015-12-04 14:18:32 +0900

    (clean-up)

---
+++ TODO.js
@@ -0,0 +1,20 @@
+/**
+
+  <TODO>
+  - bug fix
+    - it should be able to text the most recent user that signed in.
+      - do we need to save all the past users? maybe just current user is okay?
+    - update user info using PUT method instead of using POST so it won't make duplicates.
+    - why working for only Shan's phone?
+
+  - improvement
+    - consistent user info save/retrieval from DB from the first page.
+
+  - Deployment
+    - DB : firebase is better(no cost, no API implementation, easier, no server)
+    - Heroku or firebase. 
+      
+  - Native conversion
+    - Convering an app to Native using Corvoda/Ionic
+
+ */
---
+++ server/config/config.js
@@ -0,0 +1,11 @@
+/* twillio envs */
+// Your accountSid and authToken from twilio.com/user/account
+var accountSid = 'ACa3e64a2845c19bc778fc1bf37e6a3b8b';
+// var authToken = "{{ auth_token }}";  << original 
+var authToken = "0f9456051662bec5e2aa9b591655a4ba";
+
+module.exports = {
+  accountSid:  accountSid,
+  authToken: authToken
+}
+
